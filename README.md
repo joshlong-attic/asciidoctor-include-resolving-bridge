90451ff53c6b81fc5677fa3ddf51fbcb27fa6b24
Josh Long <starbuxman@gmail.com>
2018-01-27 00:18:03 -0800

    asciidoctor

--- README.md
+++
@@ -1,2 +0,0 @@
-# asciidoctor-include-resolving-bridge
-support reading in Asciidoctor docs and then manually resolving the includes in the artifacts.
---
+++ src/test/kotlin/com/example/asciidoctorpreprocessor/AsciidoctorPreprocessorApplicationTests.kt
@@ -0,0 +1,86 @@
+package com.example.asciidoctorpreprocessor
+
+import org.apache.commons.logging.LogFactory
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.ast.Document
+import org.asciidoctor.ast.DocumentRuby
+import org.asciidoctor.extension.IncludeProcessor
+import org.asciidoctor.extension.PreprocessorReader
+import org.junit.Test
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.core.io.FileSystemResource
+import org.springframework.web.client.getForEntity
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+
+// take an Asciidoctor input, manually resolve
+// the includes, and emit the pre-processed file.
+class AsciidoctorPreprocessorApplicationTests {
+
+	private val log = LogFactory.getLog(javaClass)
+
+	private val cloudNativeJavaBookRoot = File("${System.getenv()["HOME"]}/cloud-native-java-book/")
+
+	private val input: String = BufferedReader(
+			InputStreamReader(FileSystemResource("/home/jlong/Desktop/misc/sample.adoc").inputStream))
+			.use { it.readLines() }
+			.joinToString(System.lineSeparator())
+
+
+	@Test
+	fun load() {
+		val asciidoctor: Asciidoctor = Asciidoctor.Factory.create()
+				.apply {
+					this.javaExtensionRegistry().includeProcessor(SimpleIncludeProcessor())
+				}
+		val document: Document = asciidoctor.load(this.input, mapOf("safe" to "unsafe", "parse" to "false"))
+		val convert = document.convert()
+		log.info("conversion: $convert")
+		log.info(document.toString())
+
+
+	}
+}
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class SimpleIncludeProcessor : IncludeProcessor() {
+
+	private val template = RestTemplateBuilder().build()
+
+	private val log = LogFactory.getLog(javaClass)
+
+	override fun process(document: DocumentRuby,
+	                     reader: PreprocessorReader, target: String, attributes: MutableMap<String, Any>) {
+		val content = this.handlerFor(target)(target)
+		reader.push_include(content, target, target, 1, attributes)
+		this.log.debug("process($document , $reader , $target , $attributes) ")
+	}
+
+	private val handlers: Map<(String) -> Boolean, (String) -> String> =
+			mutableMapOf({ target: String -> target.contains("snippets") } to { target: String ->
+				/* todo have this actually read the file from the file system */ target })
+
+	private fun handlerFor(target: String): (String) -> String =
+			this.handlers.entries.firstOrNull { entry -> entry.key(target) }?.value ?: {
+				// default handler, checks Github
+				this.template.getForEntity<String>(this.targetToCNJGitHubUrl(it)).body
+			}
+
+	private fun targetToCNJGitHubUrl(target: String) =
+			target.replace("//", "")
+					.let {
+						val withoutDot = if (it.startsWith(".")) {
+							it.substring(1)
+						} else {
+							it
+						}
+						val dividedBySlash = withoutDot.split('/')
+						val root = dividedBySlash[0]
+						val rest = dividedBySlash.slice(IntRange(1, dividedBySlash.size - 1)).joinToString("/")
+						"https://raw.githubusercontent.com/cloud-native-java/$root/master/$rest"
+					}
+
+	override fun handles(target: String?): Boolean = true
+
+}
